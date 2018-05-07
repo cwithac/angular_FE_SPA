2bc27e54bfc4a7b1ecfb736b70d07baff34f37e5
Cathleen Wright <cathleenmwright@gmail.com>
2018-05-07 13:24:04 -0400

    214 - ng-repeat data

--- controllers/cribsController.js
+++ controllers/cribsController.js
@@ -2,6 +2,25 @@ angular
 	.module('ngCribs')
 	.controller('cribsController', function($scope) {
 
-		$scope.hello = "Hello World";
+		$scope.cribs = [
+      {
+        "type": "Condo",
+        "price": 220000,
+        "address": "213 Grove Street",
+        "description": "Excellent place, really nice view!"
+      },
+      {
+        "type": "House",
+        "price": 410500,
+        "address": "7823 Winding Way",
+        "description": "Beautiful home with lots of space for a large family."
+      },
+      {
+        "type": "Duplex",
+        "price": 39500,
+        "address": "834 River Lane",
+        "description": "Great neighborhood and lots of nice green space."
+      }
+    ];
 
 	});
