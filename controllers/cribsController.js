9161fe72b4fa6c6d20e7b57649854ba3454e2a69
Cathleen Wright <cathleenmwright@gmail.com>
2018-05-07 13:30:00 -0400

    215 Angular Factory

--- controllers/cribsController.js
+++ controllers/cribsController.js
@@ -1,26 +1,7 @@
 angular
 	.module('ngCribs')
-	.controller('cribsController', function($scope) {
+	.controller('cribsController', function($scope, cribsFactory) {
 
-		$scope.cribs = [
-      {
-        "type": "Condo",
-        "price": 220000,
-        "address": "213 Grove Street",
-        "description": "Excellent place, really nice view!"
-      },
-      {
-        "type": "House",
-        "price": 410500,
-        "address": "7823 Winding Way",
-        "description": "Beautiful home with lots of space for a large family."
-      },
-      {
-        "type": "Duplex",
-        "price": 39500,
-        "address": "834 River Lane",
-        "description": "Great neighborhood and lots of nice green space."
-      }
-    ];
+		$scope.cribs = cribsFactory.getCribs();
 
 	});
